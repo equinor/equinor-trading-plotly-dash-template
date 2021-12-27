b495cc8dc17eb9b9a37eaaa7ad8e87e0970b096c
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-27 16:21:33 +0100

    Remove sudo from startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get install -y ca-certificates
+apt-get update && sudo apt-get install -y ca-certificates
 curl -sSL https://install.python-poetry.org | python3 -
 echo "$HOME/.poetry/bin" >> $GITHUB_PATH
 poetry config virtualenvs.in-project true
