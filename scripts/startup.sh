f0a7307d09e387eb49767e8656094d25925864f0
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-27 17:08:16 +0100

    Revert back to requirements.txt

--- scripts/startup.sh
+++
@@ -1,6 +0,0 @@
-apt-get update && apt-get install -y ca-certificates
-curl --insecure -sSL https://install.python-poetry.org | python3 -
-echo "$HOME/.poetry/bin" >> $GITHUB_PATH
-poetry config virtualenvs.in-project true
-poetry install
-poetry run gunicorn --bind=0.0.0.0 --timeout 600 "app:app()"
