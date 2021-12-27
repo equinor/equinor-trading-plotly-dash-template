8f43c36b28b060875b1e312af404e823c57c3940
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-27 16:35:39 +0100

    Remove more sudos

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
-apt-get update && sudo apt-get install -y ca-certificates
+apt-get update && apt-get install -y ca-certificates
 curl -sSL https://install.python-poetry.org | python3 -
 echo "$HOME/.poetry/bin" >> $GITHUB_PATH
 poetry config virtualenvs.in-project true
