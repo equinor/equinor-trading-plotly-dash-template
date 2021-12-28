3dc6e88acc6d11220f32ebd04a2ee6ae547b5a98
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-28 09:06:05 +0100

    Update config

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ SCOPES = ["User.ReadBasic.All"]
 SESSION_TYPE = (
     "filesystem"  # Specifies the token cache should be stored in server-side session
 )
-KEYVAULT_URI = "https://vault4ebe57f9.vault.azure.net/"
+KEYVAULT_URI = "https://vaultfa34a853.vault.azure.net/"
 SECRET_NAME = "client-secret"
 ROLES: List[str] = []
 CHECK_PROD = "IS_PROD"
@@ -21,7 +21,7 @@ CHECK_PROD = "IS_PROD"
 # Data files
 data_files = {
     "iris": {
-        "account_url": "https://sa72814a2b.blob.core.windows.net",
+        "account_url": "https://sa55af3238.blob.core.windows.net",
         "container": "test",
         "filename": "iris.csv",
     }
